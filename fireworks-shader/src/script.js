391f4341d6d73cddb5975c3c273bfded226c3c2c
lliam-mcbean <lliam.mcbean@gmail.com>
2024-08-02 14:29:48 -0700

    fireworks complete

---
+++ fireworks-shader/src/script.js
@@ -0,0 +1,191 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import gsap from 'gsap'
+import fireworkVertexShader from './shaders/firework/vertex.glsl'
+import fireworkFragmentShader from './shaders/firework/fragment.glsl'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI({ width: 340 })
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Loaders
+const textureLoader = new THREE.TextureLoader()
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    pixelRatio: Math.min(window.devicePixelRatio, 2)
+}
+sizes.resolution = new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+    sizes.resolution.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(sizes.pixelRatio)
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1.5, 0, 6)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(sizes.pixelRatio)
+
+/**
+ * fireworks
+ */
+
+const textures = [
+    textureLoader.load('./particles/1.png'),
+    textureLoader.load('./particles/2.png'),
+    textureLoader.load('./particles/3.png'),
+    textureLoader.load('./particles/4.png'),
+    textureLoader.load('./particles/5.png'),
+    textureLoader.load('./particles/6.png'),
+    textureLoader.load('./particles/7.png'),
+    textureLoader.load('./particles/8.png')
+]
+
+const createFirework = (count, position, size, texture, radius, color) => {
+    const positions = new Float32Array(count * 3)
+    const sizesArray = new Float32Array(count)
+    const timeMultipliersArray = new Float32Array(count)
+
+    for (let i = 0; i < count; i++) {
+        const i3 = i*3
+
+        const spherical = new THREE.Spherical(
+            radius * (0.75 + Math.random() * 0.25),
+            Math.random() * Math.PI,
+            Math.random() * Math.PI * 2
+        )
+        const position = new THREE.Vector3()
+        position.setFromSpherical(spherical)
+
+        positions[i3] = position.x
+        positions[i3 + 1] = position.y
+        positions[i3 + 2] = position.z
+
+        sizesArray[i] = Math.random()
+
+        timeMultipliersArray[i] = 1 + Math.random()
+    }
+    const geometry = new THREE.BufferGeometry()
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
+    geometry.setAttribute('aSize', new  THREE.Float32BufferAttribute(sizesArray, 1))
+    geometry.setAttribute('aTimeMultiplier', new THREE.Float32BufferAttribute(timeMultipliersArray, 1))
+
+    texture.flipY = false
+    const material = new THREE.ShaderMaterial({
+        fragmentShader: fireworkFragmentShader,
+        vertexShader: fireworkVertexShader,
+        uniforms: {
+            uSize: new THREE.Uniform(size),
+            uResolution: new THREE.Uniform(sizes.resolution),
+            uTexture: new THREE.Uniform(texture),
+            uColor: new THREE.Uniform(color),
+            uProgress: new THREE.Uniform()
+        },
+        transparent: true,
+        depthWrite: false, 
+        blending: THREE.AdditiveBlending
+    })
+
+    const firework = new THREE.Points(geometry, material)
+    firework.position.copy(position)
+    scene.add(firework)
+
+    const destroy = () => {
+        scene.remove(firework)
+        geometry.dispose()
+        material.dispose()
+    }
+
+    gsap.to(
+        material.uniforms.uProgress,
+        {value: 1, duration: 3, ease: 'linear', onComplete: destroy}
+    )
+}
+
+createFirework(
+    100, 
+    new THREE.Vector3(), 
+    0.2,
+    textures[7],
+    1,
+    new THREE.Color('#fec76f')
+)
+
+const createRandomFirework = () =>
+    {
+        const count = Math.round(400 + Math.random() * 1000)
+        const position = new THREE.Vector3(
+            (Math.random() - 0.5) * 2,
+            Math.random(),
+            (Math.random() - 0.5) * 2
+        )
+        const size = 0.1 + Math.random() * 0.1
+        const texture = textures[Math.floor(Math.random() * textures.length)]
+        const radius = 0.5 + Math.random()
+        const color = new THREE.Color()
+        color.setHSL(Math.random(), 1, 0.7)
+        createFirework(count, position, size, texture, radius, color)
+    }
+
+window.addEventListener('click', () => {
+    createRandomFirework()
+})
+
+/**
+ * Animate
+ */
+const tick = () =>
+{
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
---
+++ fireworks-shader/src/shaders/firework/vertex.glsl
@@ -0,0 +1,47 @@
+uniform float uSize;
+uniform vec2 uResolution;
+uniform float uProgress;
+
+attribute float aSize;
+attribute float aTimeMultiplier;
+
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
+{
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
+
+void main(){
+    float progress = uProgress * aTimeMultiplier;
+    vec3 newPosition = position;
+
+    // exploding
+    float explodingProgress = remap(progress,  0.0, 0.1, 0.0, 1.0);
+    explodingProgress = clamp(explodingProgress, 0.0, 1.0);
+    explodingProgress = 1.0 - pow(1.0 - explodingProgress, 3.0);
+    newPosition *= explodingProgress;
+
+    // falling 
+    float fallingProgress = remap(progress, 0.1, 1.0, 0.0, 1.0);
+    fallingProgress = clamp(fallingProgress, 0.0, 1.0);
+    fallingProgress = 1.0 - pow(1.0 - fallingProgress, 3.0);
+    newPosition.y -= fallingProgress * 0.2;
+
+    // scaling
+    float sizeOpeningProgress = remap(progress, 0.0, 0.125, 0.0, 1.0);
+    float sizeClosingProgress = remap(progress, 0.125, 1.0, 1.0, 0.0);
+    float sizeProgress = min(sizeOpeningProgress, sizeClosingProgress);
+    sizeProgress = clamp(sizeProgress, 0.0, 1.0);
+
+    // twinkling
+    float twinklingProgress = remap(progress, 0.2, 0.0, 0.0, 1.0);
+    twinklingProgress = clamp(twinklingProgress, 0.0, 1.0);
+    float sizeTwinkling = sin(progress * 30.0) * 0.5 + 0.5;
+    sizeTwinkling = 1.0 - sizeTwinkling * twinklingProgress;
+
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    gl_Position = projectionMatrix * viewPosition;
+
+    gl_PointSize = uSize * uResolution.y * aSize * sizeProgress * sizeTwinkling;
+    gl_PointSize *= 1.0 / -viewPosition.z;
+}
